ae6da36eb56d8d19ef1f6ad79b086491ff1c1a96
Jon <jsmaxi666@gmail.com>
2025-02-13 12:37:05 +0200

    Install dependencies in shuttle runtime

--- agent/shuttle_setup_container.sh
+++ agent/shuttle_setup_container.sh
@@ -1,5 +1,32 @@
+# Update the package list
+echo "Updating package list..."
+apt-get update
+
+# Install Git
+echo "Installing Git..."
+apt-get install -y git
+
+# Install curl (required for Rust installation)
+echo "Installing curl..."
+apt-get install -y curl
+
+# Install Rust using rustup (which includes cargo)
+echo "Installing Rust and Cargo..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Add Cargo to the current shell session
+source $HOME/.cargo/env
+
+# Verify installations
+echo "Verifying installations..."
+git --version
+cargo --version
+
 # install movement cli: https://docs.movementnetwork.xyz/devs/movementcli
+echo "Installing Movement CLI..."
 git clone https://github.com/movementlabsxyz/aptos-core/ && cd aptos-core
 cargo build -p movement
-sudo cp target/debug/movement /usr/local/bin/
+cp target/debug/movement /usr/local/bin/
 movement --help
+
+echo "Installation done"
